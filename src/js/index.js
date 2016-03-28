89c1b40520d1a8d97e1abefdc8eac640a66378bb
Roy Hewitt <roy_hewitt@fws.gov>
2016-03-28 08:44:19 -0400

    Coordinate frontend modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,58 @@
 (function () {
   'use strict';
-
   var zenscroll = require('zenscroll');
-
-  var emitter = require('./mediator');
+  var Parallaxify = require('parallaxify');
+  var ImageComparison = require('./imageComparison');
+  var fitText = require('./fittext');
   var dots = require('./dots');
+  var nav = require('./nav');
+  // var graph = require('./graph');
 
-  dots.init();
+  // Scroll speed and offset (offset is for fixed navigation)
+  zenscroll.setup(777, 50);
 
-  console.log('WOOOO');
+  var sr = require('scrollreveal')({ reset: true });
+
+  new Parallaxify().registerUpdate();
+  sr.reveal('.reveal');
+  fitText( document.getElementsByClassName('section-heading'));
+
+  nav.init({
+    target: document.querySelector('.mini-nav'),
+    headings: '.section-heading'
+  });
+
+  dots.init({
+    container: document.getElementById('content'),
+    className: 'dot-nav',
+    showHeadline: false
+  });
+
+  // graph.init();
+
+  document.addEventListener('DOMContentLoaded', domReady);
+
+  function domReady() {
+
+    var imageComparisonCollection = document.querySelectorAll('.image-comparison');
+    for (var i = 0; i < imageComparisonCollection.length; i++) {
+      var imageWidget = imageComparisonCollection[i];
+      var images = imageWidget.querySelectorAll('.comparison-image');
+      new window.ImageComparison({
+        container: imageWidget,
+        startPosition: 50,
+        data: [
+          {
+            image: images[0],
+            label: 'Year: 2009'
+          },
+          {
+            image: images[1],
+            label: 'Year: 2060'
+          }
+        ],
+      });
+    }
+  }
+
 })();
