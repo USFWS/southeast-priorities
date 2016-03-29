2e141185972be13aba5869b45c8094f8ad14a165
Roy Hewitt <roy_hewitt@fws.gov>
2016-03-29 07:44:12 -0400

    Scale down images and run them through optomizer

---
+++ build/resize-img.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+  var sharp = require('sharp');
+  var Imagemin = require('imagemin');
+  var rename = require('gulp-rename');
+  var mkdirp = require('mkdirp');
+  var fs = require('fs');
+
+  var input = 'src/images/other/';
+  var output = 'site/static/images/';
+  var images = fs.readdirSync(input);
+
+  // If there's a DS Store item, remove it
+  var i = images.indexOf('.DS_Store');
+  if (i > -1) images.splice(i,1);
+
+  // Ensure the output dir exists
+  mkdirp(output, function(err) {
+    if (err) console.error(err);
+
+    images.forEach(function (name) {
+      var img = sharp(input + name);
+      img
+        .resize(700)
+        .toBuffer(function (err, buffer, info) {
+          if (err) console.error(err);
+          jpegmin(buffer, name);
+        });
+    });
+  });
+
+  function jpegmin(buffer, name) {
+    new Imagemin()
+      .src(buffer)
+      .dest(output)
+      .use(Imagemin.jpegtran({ progressive: true }))
+      .use(rename(name))
+      .run();
+  }
+
+})();
